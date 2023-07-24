55912e77ffcbe0dbd0cdab55a3e51e01d00546d1
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-07-24 08:15:35 -0600

    Update main.py
    Created initial main for testing the Github enviroment

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, jsonify
 
+#Flask constructor
+app = Flask[__name__]
+
+#The route that the application should call the associated function
+@app.route('/')
+
+#Testing function to check enviroment
+def test():
+  return 'Test'
+
+if __name__ == '__main__':
+  app.run()
