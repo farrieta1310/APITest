de75d0b3dc3b8be87e4a316c4aa114eaa427e5f1
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-07-24 08:58:12 -0600

    Update main.py
    Changes: 
    - Added import for csv library
    - Adjusted app route and changed the method to POST
    - Changed main function name
    - Added logic to take in file and assign it to to var
    - Added code to test the the POST request completed

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import csv
 from flask import Flask, request, jsonify
 
 #Flask constructor
-app = Flask[__name__]
+app = Flask(__name__)
 
 #The route that the application should call the associated function
-@app.route('/')
+@app.route('/upload', methods=['POST'])
 
-#Testing function to check enviroment
-def test():
-  return 'Test'
+#Function to Upload File through API
+def upload_file():
+  try:
+    #read csv file
+    file = request.files['file']
+    reader = csv.reader(file)
+    return jsonify({"message":'read file'}),201
+  except Exception as e:
+    return jsonify({"error":str(e)}),500
 
 if __name__ == '__main__':
   app.run()
