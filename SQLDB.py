9c602545766acc6651038c948df48d9c4e15a369
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-07-25 17:09:10 -0600

    Update SQLDB.py
    Added function to export the Departments table to AVRO files

--- SQLDB.py
+++ SQLDB.py
@@ -1,5 +1,9 @@
 import pyodbc
 from GoogleSecretManager import GoogleSecretsManager
+import avro.schema
+from avro.datafile import DataFileWriter
+from avro.io import DatumWriter
+import json
 
 class SQLDB:
     def __init__(self):
@@ -34,3 +38,32 @@ class SQLDB:
         except pyodbc.Error as e:
             print(f"Error executing the query: {e}")
             return None
+            
+    def export_table_to_avro(self, table_name, avro_format ,output_file):
+        try:
+            #Parse the schema so we can use it to write the data            
+            avro_schema = avro.schema.parse(open(avro_format).read())
+            cursor = self.connection.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            table_data = cursor.fetchall()   
+            print(table_data)      
+
+            # Create Avro data file and write data to it
+            with open(output_file, 'wb') as avro_file:
+                writer = DataFileWriter(avro_file, DatumWriter(), avro_schema)
+                for row in table_data:
+                    department_id, department_name = row
+                    writer.append({"Department_id": department_id, "Department_Name": department_name})
+                writer.close()
+
+            return 'Table exported succesfully'
+        except pyodbc.Error as e:
+            print(f"Error exporting table: {e}")
+            return None        
+        
+# Example Export:
+if __name__ == "__main__":
+    db_sql = SQLDB()
+    db_sql.connect()
+    db_sql.export_table_to_avro(table_name='DEPARTMENTS', avro_format='avro_department_format.avsc' ,output_file='department_output.avro')        
+    db_sql.disconnect()
