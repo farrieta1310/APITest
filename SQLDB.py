629e85e2a3c4430a316917b6050f99585a76903b
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-07-25 17:51:51 -0600

    Update SQLDB.py
    Added logic to export/import the Hired Employees data

--- SQLDB.py
+++ SQLDB.py
@@ -3,6 +3,7 @@ from GoogleSecretManager import GoogleSecretsManager
 import avro.schema
 from avro.datafile import DataFileWriter, DataFileReader
 from avro.io import DatumWriter, DatumReader
+import datetime
 
 class SQLDB:
     def __init__(self):
@@ -38,7 +39,7 @@ class SQLDB:
             print(f"Error executing the query: {e}")
             return None
             
-    def export_table_to_avro(self, table_name, avro_format ,output_file):
+    def export_department_table_to_avro(self, table_name, avro_format ,output_file):
         try:
             #Parse the schema so we can use it to write the data            
             avro_schema = avro.schema.parse(open(avro_format).read())
@@ -60,7 +61,51 @@ class SQLDB:
             print(f"Error exporting table: {e}")
             return None        
 
-    def insert_from_avro(self, avro_file):
+    def export_jobs_table_to_avro(self, table_name, avro_format ,output_file):
+        try:
+            #Parse the schema so we can use it to write the data            
+            avro_schema = avro.schema.parse(open(avro_format).read())
+            cursor = self.connection.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            table_data = cursor.fetchall()   
+            print(table_data)      
+
+            # Create Avro data file and write data to it
+            with open(output_file, 'wb') as avro_file:
+                writer = DataFileWriter(avro_file, DatumWriter(), avro_schema)
+                for row in table_data:
+                    department_id, department_name = row
+                    writer.append({"Jobs_Id": department_id, "Department_Name": department_name})
+                writer.close()
+
+            return 'Table exported succesfully'
+        except pyodbc.Error as e:
+            print(f"Error exporting table: {e}")
+            return None        
+
+    def export_hired_employees_table_to_avro(self, table_name, avro_format ,output_file):
+        try:
+            #Parse the schema so we can use it to write the data            
+            avro_schema = avro.schema.parse(open(avro_format).read())
+            cursor = self.connection.cursor()
+            cursor.execute(f"SELECT ID, NAME, CONVERT(VARCHAR, HIRE_DATE, 121) AS HIRE_DATE, DEPARTMENT_ID, JOBS_ID FROM {table_name}")
+            table_data = cursor.fetchall()   
+            print(table_data)      
+
+            # Create Avro data file and write data to it
+            with open(output_file, 'wb') as avro_file:
+                writer = DataFileWriter(avro_file, DatumWriter(), avro_schema)
+                for row in table_data:
+                    id, name, hire_date, department_id, job_id  = row      
+                    writer.append({"Id": id, "Name": name, "Hire_Date": hire_date, "Department_id":department_id, "Jobs_id":job_id})
+                writer.close()
+
+            return 'Table exported succesfully'
+        except pyodbc.Error as e:
+            print(f"Error exporting table: {e}")
+            return None  
+
+    def insert_department_from_avro(self, avro_file):
         # Read data from the Avro file and insert into the database
         with open(avro_file, 'rb') as avro_file:
             reader = DataFileReader(avro_file, DatumReader())
@@ -70,10 +115,25 @@ class SQLDB:
                 self.execute_query(f"EXEC dbo.ADD_DEPARTMENTS @department_id ={department_id},@department='{department_name}'") 
             reader.close()
 
-# Example Export:
-if __name__ == "__main__":
-    db_sql = SQLDB()
-    db_sql.connect()
-    #db_sql.export_table_to_avro(table_name='DEPARTMENTS', avro_format='avro_department_format.avsc' ,output_file='department_output.avro')        
-    db_sql.insert_from_avro(avro_file='department_output.avro')
-    db_sql.disconnect()
+    def insert_job_from_avro(self, avro_file):
+        # Read data from the Avro file and insert into the database
+        with open(avro_file, 'rb') as avro_file:
+            reader = DataFileReader(avro_file, DatumReader())
+            for record in reader:
+                job_id = record["Jobs_Id"]
+                department_name = record["Department_Name"]
+                self.execute_query(f"EXEC dbo.ADD_JOBS @job_id ={job_id},@department='{department_name}'") 
+            reader.close()            
+
+    def insert_employees_from_avro(self, avro_file):
+        # Read data from the Avro file and insert into the database
+        with open(avro_file, 'rb') as avro_file:
+            reader = DataFileReader(avro_file, DatumReader())
+            for record in reader:
+                id = record["Id"]
+                name = record["Name"]
+                hire_date = record["Hire_Date"]
+                department_id = record["Department_id"]
+                job_id = record["Jobs_id"]                                                
+                self.execute_query(f"EXEC dbo.ADD_HIRED_EMPLOYEES @id={id},@name='{name}',@hire_date='{hire_date}',@department_id='{department_id}',@job_id='{job_id}'") 
+            reader.close()  
